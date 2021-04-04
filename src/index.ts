f0467bd415cdae413470c4d825f352c84e308da3
Sri Harsha Chinni <sriharshachinni@Harshas-MacBook-Pro.local>
2021-04-04 17:48:37 -0600

    Formats the index file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
+
 const app = express();
 const {
     PORT = 3000,
 } = process.env;
+
 app.get('/', (req: Request, res: Response) => {
     res.send({
         message: 'API index page',

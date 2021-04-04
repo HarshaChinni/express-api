68421ca1e6107de0dfad0464e718a9c9cc7b2451
Sri Harsha Chinni <sriharshachinni@Harshas-MacBook-Pro.local>
2021-04-04 17:18:51 -0600

    Initial project setup - Express REST API

---
+++ README.md
@@ -0,0 +1,3 @@
+# Express REST API project
+
+Mock project to build a simple REST API server
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const { NODE_ENV = "production" } = process.env;
+module.exports = {
+  entry: "./src/index.ts",
+  mode: NODE_ENV,
+  target: "node",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js"
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  }
+};

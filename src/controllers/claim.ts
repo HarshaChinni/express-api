09e0d4c35cc88e8b96a59b82b94000c4b8f558e4
Sri Harsha Chinni <sriharshachinni@Harshas-MacBook-Pro.local>
2021-04-04 19:50:59 -0600

    Adds Claims controller - Performs Core business logic and checks

---
+++ src/controllers/claim.ts
@@ -0,0 +1,51 @@
+import { ALL_CLAIMS } from "../utils/mock-data";
+import { ClaimDetails } from "../models/cliam-model";
+import { randomNumber } from "../utils/util";
+
+
+export async function createClaim(claimDetails: ClaimDetails) {
+    // To replicate the DB operation is performed
+    setTimeout(() => { }, 200);
+    const { desc, itemDetails } = claimDetails;
+    // const query = INSERT INTO Claims(desc, item_json) VALUES (desc, itemDetails)
+    // Log Info after the query is successfully completed
+
+    return {
+        claimId: randomNumber(1, 1000),
+        desc,
+        itemDetails
+    }
+}
+
+
+export const getClaimById = (claimId) => {
+    // Log Info statement
+
+    // verify if the claimId is exists in the DB
+
+    if (!isValidClaimId(claimId)) {
+        // Log Error statement
+        throw Error(`Invalid claim ID ${claimId} passed `);
+    }
+
+    return buildClaim(claimId)
+}
+
+
+export const getAllClaims = () => {
+    return ALL_CLAIMS;
+}
+
+
+function isValidClaimId(claimId) {
+    // const query = select count(claimId) from Claims where claim_id = claimId;
+    const query_result = 1;
+    return query_result > 0;
+}
+
+
+function buildClaim(claimId) {
+    // const query = SELECT * FROM CLAIMS;
+
+    return ALL_CLAIMS[0];
+}
